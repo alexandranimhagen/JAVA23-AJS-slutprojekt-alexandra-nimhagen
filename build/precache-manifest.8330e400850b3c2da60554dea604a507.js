9b9259339b0f2d8f764b4ab1cb068beb6af1aee7
alexandranimhagen <nimhagen@hotmail.com>
2024-06-07 18:17:09 +0200

    Build project

---
+++ build/precache-manifest.8330e400850b3c2da60554dea604a507.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd101c072e74df0a7d3212bcd2efc9d4",
+    "url": "/JAVA23-AJS-slutprojekt-alexandra-nimhagen/index.html"
+  },
+  {
+    "revision": "5b739f06b6d4b64e69d9",
+    "url": "/JAVA23-AJS-slutprojekt-alexandra-nimhagen/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "5d7880c60b44b2947e47",
+    "url": "/JAVA23-AJS-slutprojekt-alexandra-nimhagen/static/css/main.1403f4fc.chunk.css"
+  },
+  {
+    "revision": "5b739f06b6d4b64e69d9",
+    "url": "/JAVA23-AJS-slutprojekt-alexandra-nimhagen/static/js/2.1bb95274.chunk.js"
+  },
+  {
+    "revision": "6dbcd2400c9d70de78ff",
+    "url": "/JAVA23-AJS-slutprojekt-alexandra-nimhagen/static/js/3.23358222.chunk.js"
+  },
+  {
+    "revision": "5d7880c60b44b2947e47",
+    "url": "/JAVA23-AJS-slutprojekt-alexandra-nimhagen/static/js/main.f03eec3e.chunk.js"
+  },
+  {
+    "revision": "05ab5d09d2e1fb137db3",
+    "url": "/JAVA23-AJS-slutprojekt-alexandra-nimhagen/static/js/runtime~main.c0496b2e.js"
+  }
+]);
